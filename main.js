11f4abf91292338d47c27d4191876c0505ac6107
Sergei Rozanov <laska4723@gmail.com>
2025-05-25 20:10:56 +0300

    Строки. Задача 5

--- main.js
+++ main.js
@@ -1,4 +1,2 @@
-const str1 = 'привет';
-const str2 = 'ветеринар';
-console.log(str1.startsWith('вет'));
-console.log(str2.startsWith('вет'));
+const text = 'this_is_an_example_string';
+console.log(text.split('_'));
