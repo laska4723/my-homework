48ec6517e9290c243008b839dc9835c1ee4e2759
Sergei Rozanov <laska4723@gmail.com>
2025-05-25 13:16:00 +0300

    Строки. Задача 3

--- main.js
+++ main.js
@@ -1,6 +1,3 @@
-let str = 'ARCTIC';
-console.log("Первый символ строки: ", str.charAt(0));
-console.log("Последний символ строки: ", str.charAt(str.length - 1));
-str = str.toLowerCase();
-const result = str[0].toUpperCase() + str.slice(1, -1) + str[str.length - 1].toUpperCase();
-console.log(result);
+const str = 'молоко';
+console.log("Индекс первой буквы о: ", str.indexOf('о'));
+console.log("Индекс последней буквы о: ", str.indexOf('о', str.length - 1));
