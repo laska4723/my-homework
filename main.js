6d668463dffd972641981ea9aee0c9d9a5f21d13
Sergei Rozanov <laska4723@gmail.com>
2025-05-28 03:37:07 +0300

    Switch. Задача 1

--- main.js
+++ main.js
@@ -1,3 +1,19 @@
-const num = Math.floor(Math.random() * ((2120 - 1980) / 10 + 1)) * 10 + 1980;
-console.log(num);
-console.log(`год ${(num % 400 === 0) || (num % 4 === 0 && num % 100 !== 0) ? "" : "не "}високосный`);
+const value = 'admin';
+
+switch (value) {
+    case 'user':
+    case 'client':
+        result = "1 Уровень";
+        break;
+
+    case 'admin':
+    case 'manager':
+        result = "2 Уровень";
+        break;
+
+    case 'superadmin':
+        result = "3 Уровень";
+        break;
+}
+
+console.log(result);
