8d2abfb4f864bea186b10931272a4b758c0e6cc4
Sergei Rozanov <laska4723@gmail.com>
2025-06-01 01:15:40 +0300

    Базовые операторы. Задача 1 (fix 1)

--- main.js
+++ main.js
@@ -1,28 +1,3 @@
-const person1 = {name: 'Sergei', surname: 'Petrov'};
-
-person1.dateOfBirth = '31-12-1995';
-person1['>:-)'] = 'devil';
-
-delete person1.dateOfBirth;
-delete person1['>:-)'];
-
-person1.contact = {phone: '+998 901752397', email: 'otvertka1337@mail.ru'};
-person1.contact.telegram = 'https://t.me/seregakot457';
-person1.contact.telegram = 'https://t.me/seregakot647';
-
-const locationType = 'city';
-const city1 = 'Moscow';
-person1[locationType] = city1;
-
-const name = 'Sergei';
-const surname = 'Ivanov';
-const dateOfBirth = '01-01-2000';
-const city2 = 'Saint-Petersburg';
-
-const person2 = {name, surname, dateOfBirth, city2};
-
-console.log(person1);
-console.log(' ');
-console.log(person2);
-
-debugger
+const a = 1 + Math.floor(Math.random() * 10);
+const s = a * a;
+console.log(`Площадь квадрата со стороной a=${a} равна ${s}`);
