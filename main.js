a79fdfe7e2aca822591108bc9d6a6b479d990fe8
Sergei Rozanov <laska4723@gmail.com>
2025-06-01 12:10:06 +0300

    Циклы. Задача 3

--- main.js
+++ main.js
@@ -1,20 +1,12 @@
-const array = [-8, 6, 7, -10, 8, 0, -3, 9];
+let num1 = 1 + Math.floor(Math.random() * 6);
+let num2 = 1 + Math.floor(Math.random() * 6);
 
-let min = array[0];
-let max = array[0];
-let sum = array[0];
-for (let i = 1; i < array.length; i++) {
-    if (array[i] < min) {
-        min = array[i];
-    }
-    if (array[i] > max) {
-        max = array[i];
-    }
-    sum += array[i];
+let count = 1;
+while (num1 !== num2) {
+    num1 = 1 + Math.floor(Math.random() * 6);
+    num2 = 1 + Math.floor(Math.random() * 6);
+    count++;
 }
 
-const avg = sum / array.length;
-
-console.log(`Минимальное число: ${min}
-Максимальное число: ${max}
-Среднее между всеми значениями: ${avg}`);
+console.log(`Выпал дубль: ${num1} ${num2}
+Количество попыток: ${count}`);
