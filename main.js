0d4db9e712c39c1e7c536586b276fafe6986baf7
Sergei Rozanov <laska4723@gmail.com>
2025-06-01 05:41:18 +0300

    Условия. Задача 2 (fix 1)

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
-const num = -2 + Math.floor(Math.random() * 5);
+const num = 1 + Math.floor(Math.random() * 20);
 console.log("Число:", num);
-if (num > 0) {
-    console.log("положительное");
-} else if (num < 0) {
-    console.log("отрицательное");
-} else {
-    console.log("null");
+if (num % 3 === 0 && num % 5 === 0) {
+    console.log("FizzBuzz");
+} else if (num % 3 === 0) {
+    console.log("Fizz");
+} else if (num % 5 === 0) {
+    console.log("Buzz");
 }
