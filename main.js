c0abacb28d217ccb8fb6937c4e1d5b8080518004
Sergei Rozanov <laska4723@gmail.com>
2025-06-01 08:04:13 +0300

    Циклы. Задача 1

--- main.js
+++ main.js
@@ -1,10 +1,16 @@
-const user = {
-    name: 'Alex',
-    nickname: 'Alexxx675',
-    nicknameHistory: ['AlexLuk', 'Xela-123', 'Sasha 8'],
-};
+const array = [-5, 6, 7, -10, 8, 0, -3, 2];
 
-user.nicknameHistory.push(user.nickname);
-user.nickname = 'bmw-e39';
-
-console.log(user);
+let positiveCount = 0;
+let negativeCount = 0;
+for (let i = 0; i < array.length; i++) {
+    if (i === array.length - 2) {
+        continue;
+    }
+    if (array[i] > 0) {
+        positiveCount++;
+    } else if (array[i] < 0) {
+        negativeCount++;
+    }
+}
+console.log(`Количество положительных чисел: ${positiveCount}
+Количество отрицательных чисел: ${negativeCount}`);
