3c58fcf8ea40d4d7c6e8a3f0d7ad5f1b85aee8fa
Sergei Rozanov <laska4723@gmail.com>
2025-06-02 13:08:08 +0300

    Циклы. Задача 3 (fix 1)

--- main.js
+++ main.js
@@ -1,48 +1,11 @@
-/*
-С помощью цикла отфильтруйте только подходящих по таким критериям людей:
-Мужчины старше 18 или женщины старше 20
+let num1;
+let num2;
+let count = 0;
+do {
+    count++;
+    num1 = 1 + Math.floor(Math.random() * 6);
+    num2 = 1 + Math.floor(Math.random() * 6);
+} while (num1 !== num2);
 
-Пол определяется по значению в поле gender:
-* f - female, женщина
-* m - man, мужчина
-
-Имена подходящих людей запишите в result, чтобы по итогу работы программы в переменной result было записано следующее:
-{
-  men: [ 'Александр', 'Дмитрий' ],
-  women: [ 'Мария', 'Екатерина', 'Наталья' ]
-}
- */
-
-const people = [
-    { id: 8, name: 'Александр', gender: 'm', age: 25 },
-    { id: 12, name: 'Мария', gender: 'f', age: 22 },
-    { id: 31, name: 'Иван', gender: 'm', age: 17 },
-    { id: 34, name: 'Ольга', gender: 'f', age: 19 },
-    { id: 53, name: 'Дмитрий', gender: 'm', age: 30 },
-    { id: 95, name: 'Екатерина', gender: 'f', age: 21 },
-    { id: 3, name: 'Сергей', gender: 'm', age: 18 },
-    { id: 20, name: 'Анна', gender: 'f', age: 20 },
-    { id: 19, name: 'Андрей', gender: 'm', age: 15 },
-    { id: 30, name: 'Наталья', gender: 'f', age: 25 },
-];
-
-const result = {
-    men: [],
-    women: [],
-};
-
-for (let i = 0; i < people.length; i++) {
-    if (people[i].gender === 'm' && people[i].age > 18) {
-        result.men.push(people[i].name);
-    } else if (people[i].gender === 'f' && people[i].age > 20) {
-        result.women.push(people[i].name);
-    }
-}
-
-console.log(result);
-/* Должно вывестись:
-{
-  men: [ 'Александр', 'Дмитрий' ],
-  women: [ 'Мария', 'Екатерина', 'Наталья' ]
-}
- */
+console.log(`Выпал дубль: ${num1} ${num2}
+Количество попыток: ${count}`);
