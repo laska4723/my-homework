4b1a98a39e763bd10182d71ffa0aa1df001498d8
Sergei Rozanov <laska4723@gmail.com>
2025-06-04 05:18:24 +0300

    Циклы. Задача 7

--- main.js
+++ main.js
@@ -1,11 +1,17 @@
-let num1;
-let num2;
-let count = 0;
-do {
-    count++;
-    num1 = 1 + Math.floor(Math.random() * 6);
-    num2 = 1 + Math.floor(Math.random() * 6);
-} while (num1 !== num2);
+const numbers1 = [1.1 , 2.2, -2, 3.3, -1, 5.5, 5.99, 7.49, 7.9999, -2.2, -3];
 
-console.log(`Выпал дубль: ${num1} ${num2}
-Количество попыток: ${count}`);
+let positiveIndices = [];
+for (let i = 0; i < numbers1.length; i++) {
+  if (numbers1[i] > 0) positiveIndices.push(i);
+}
+
+const excludeIndices = positiveIndices.slice(-3);
+
+let result = [];
+for (let i = 0; i < numbers1.length; i++) {
+  if (numbers1[i] > 0 && !excludeIndices.includes(i)) {
+    result.push(Math.round(numbers1[i]));
+  } else result.push(numbers1[i]);
+}
+
+console.log(result);
