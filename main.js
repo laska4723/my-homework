65a7bcd587fbe0c210ac4d607a14034c5d145bba
Sergei Rozanov <laska4723@gmail.com>
2025-06-06 02:32:39 +0300

    Строки. Задача 2 (fix 1)

--- main.js
+++ main.js
@@ -1,4 +1,10 @@
-const str = 'arctic';
+let str1 = 'ARCTIC';
 
-console.log(`Длина строки: ${str.length}
-Строка в заглавном регистре: ${'arctic'.toUpperCase()}`);
+console.log(`Первый символ строки: ${str1.charAt(0)}
+Последний символ строки: ${str1.charAt(str1.length - 1)}`);
+
+str1 = str1.toLowerCase();
+
+const str2 = str1[0].toUpperCase() + str1.slice(1, -1) + str1[str1.length - 1].toUpperCase();
+
+console.log(str2);
