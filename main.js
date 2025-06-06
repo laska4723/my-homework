4a38261e9caa7a543b35c74ccafae7efd6b97494
Sergei Rozanov <laska4723@gmail.com>
2025-06-06 08:37:56 +0300

    Строки. Задача 3 (fix 1)

--- main.js
+++ main.js
@@ -1,10 +1,4 @@
-let str1 = 'ARCTIC';
+const str = 'молоко';
 
-console.log(`Первый символ строки: ${str1.charAt(0)}
-Последний символ строки: ${str1.charAt(str1.length - 1)}`);
-
-str1 = str1.toLowerCase();
-
-const str2 = str1[0].toUpperCase() + str1.slice(1, -1) + str1[str1.length - 1].toUpperCase();
-
-console.log(str2);
+console.log(`Индекс первой буквы о: ${str.indexOf('о')}
+Индекс последней буквы о: ${str.indexOf('о', str.length - 1)}`);
