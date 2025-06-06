15fbfb2536c479971c070aaa5777827e0391b9e8
Sergei Rozanov <laska4723@gmail.com>
2025-06-06 08:51:05 +0300

    Строки. Задача 4 (fix 1)

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
 const str = 'молоко';
 
-console.log(`Индекс первой буквы о: ${str.indexOf('о')}
-Индекс последней буквы о: ${str.indexOf('о', str.length - 1)}`);
+console.log(`${str.includes('у')}
+${str.includes('м')}`);
