6773808cd450384bc57f8a8f5cf333f963e07d9b
Sergei Rozanov <laska4723@gmail.com>
2025-06-07 09:22:23 +0300

    Циклы. Задача 1 (fix 1)

--- main.js
+++ main.js
@@ -1,5 +1,17 @@
-const name = 'Ruslan';
-const message = `Добро пожаловать, ${name}, благодарим за регистрацию!
-Надеемся, Вам понравится`;
+const array = [-5, 6, 7, -10, 8, 0, -3, 2];
 
-console.log(message);
+let positiveCount = 0;
+let negativeCount = 0;
+for (let i = 0; i < array.length; i++) {
+  const element = array[i];
+  if (i === array.length - 2) {
+    continue;
+  }
+  if (element > 0) {
+    positiveCount++;
+  } else if (element < 0) {
+    negativeCount++;
+  }
+}
+console.log(`Количество положительных чисел: ${positiveCount}
+Количество отрицательных чисел: ${negativeCount}`);
