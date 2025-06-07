4b1d5bfb73f7607d980a0472da7afd48888417fa
Sergei Rozanov <laska4723@gmail.com>
2025-06-07 12:12:49 +0300

    Циклы. Задача 7 (fix 1)

--- main.js
+++ main.js
@@ -1,53 +1,16 @@
-/*
-С помощью цикла отфильтруйте только подходящих по таким критериям людей:
-Мужчины старше 18 или женщины старше 20
+const numbers = [1.1 , 2.2, -2, 3.3, -1, 5.5, 5.99, 7.49, 7.9999, -2.2, -3];
 
-Пол определяется по значению в поле gender:
-* f - female, женщина
-* m - man, мужчина
+let positiveCount = 0;
+for (let i = numbers.length - 1; i >= 0; i--) {
+  const element = numbers[i];
 
-Имена подходящих людей запишите в result, чтобы по итогу работы программы в переменной result было записано следующее:
-{
-  men: [ 'Александр', 'Дмитрий' ],
-  women: [ 'Мария', 'Екатерина', 'Наталья' ]
-}
- */
-
-const people = [
-  { id: 8, name: 'Александр', gender: 'm', age: 25 },
-  { id: 12, name: 'Мария', gender: 'f', age: 22 },
-  { id: 31, name: 'Иван', gender: 'm', age: 17 },
-  { id: 34, name: 'Ольга', gender: 'f', age: 19 },
-  { id: 53, name: 'Дмитрий', gender: 'm', age: 30 },
-  { id: 95, name: 'Екатерина', gender: 'f', age: 21 },
-  { id: 3, name: 'Сергей', gender: 'm', age: 18 },
-  { id: 20, name: 'Анна', gender: 'f', age: 20 },
-  { id: 19, name: 'Андрей', gender: 'm', age: 15 },
-  { id: 30, name: 'Наталья', gender: 'f', age: 25 },
-];
-
-const result = {
-  men: [],
-  women: [],
-};
-
-for (let i = 0; i < people.length; i++) {
-  const person = people[i];
-  const name = person.name;
-  const gender = person.gender;
-  const age = person.age;
-
-  if (gender === 'm' && age > 18) {
-    result.men.push(name);
-  } else if (gender === 'f' && age > 20) {
-    result.women.push(name);
+  if (element > 0) {
+    if (positiveCount < 3) {
+      positiveCount++;
+    } else {
+      numbers[i] = Math.round(element);
+    }
   }
 }
 
-console.log(result);
-/* Должно вывестись:
-{
-  men: [ 'Александр', 'Дмитрий' ],
-  women: [ 'Мария', 'Екатерина', 'Наталья' ]
-}
- */
+console.log(numbers);
