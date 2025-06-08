8103a56ebf878f8d9027baaf386ea0d3e589aeab
Sergei Rozanov <laska4723@gmail.com>
2025-06-08 10:30:25 +0300

    Циклы. Задача 7 (fix 2)

--- main.js
+++ main.js
@@ -1,8 +1,16 @@
-const h = 8;
+const numbers = [1.1, -2.2, -2, 3.3, -1, 5.5, 5.99, 7.49, 7.9999, -2.2, -3];
 
-for (let i = 1; i <= h; i++) {
-  const space = ' '.repeat(h - i);
-  const star = '*'.repeat(i * 2 - 1);
+let positiveCount = 0;
+for (let i = numbers.length - 1; i >= 0; i--) {
+  const element = numbers[i];
 
-  console.log(space + star);
+  if (element > 0) {
+    if (positiveCount < 3) {
+      positiveCount++;
+      continue;
+    }
+  numbers[i] = Math.round(element);
+  }
 }
+
+console.log(numbers);
