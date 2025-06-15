f266581a7db23cd9072a5be12b6422e95da1383e
Sergei Rozanov <laska4723@gmail.com>
2025-06-15 10:57:41 +0300

    Составные типы. Задача 3

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,15 @@
 type Gender = 'male' | 'female' | 'prefer_not_to_answer';
 
 type GenderCode = 0 | 1 | 2;
+
+type UserProfile = {
+  gender: Gender;
+  genderCode: GenderCode;
+}
+
+const user: UserProfile = {
+  gender: 'male',
+  genderCode: 0
+}
+
+console.log(user);
