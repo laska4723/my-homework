f377170a2fea00e445a582a6de42c8113ccf8e54
Sergei Rozanov <laska4723@gmail.com>
2025-06-16 15:20:14 +0300

    Составные типы. Задача 5

--- src/main.ts
+++ src/main.ts
@@ -1,21 +1,19 @@
-type Gender = 'male' | 'female' | 'prefer_not_to_answer';
+import dayjs from 'dayjs';
 
-type GenderCode = 0 | 1 | 2;
+console.log(`1 группа:
+12. ${new Date('2024-08-23T12:30:00Z').toISOString()}
+2. ${dayjs('2024-08-23T15:30:00+03:00').toISOString()}
+5. ${dayjs('2024-08-24T01:30:00+13:00').toISOString()}
+9. ${dayjs('2024-08-23T09:30:00-03:00').toISOString()}
 
-type UserProfile = {
-  gender: Gender;
-  genderCode: GenderCode;
-  createdAt: Date;
-  createdAtISO: string
-}
+2 группа:
+3. ${new Date('2024-08-23T14:30:00Z').toISOString()}
+11. ${dayjs('2024-08-24T02:30:00+12:00').toISOString()}
+1. ${dayjs('2024-08-23T03:30:00-11:00').toISOString()}
+7. ${dayjs('2024-08-23T10:30:00-04:00').toISOString()}
 
-const now = new Date();
-
-const user: UserProfile = {
-  gender: 'male',
-  genderCode: 0,
-  createdAt: now,
-  createdAtISO: now.toISOString()
-}
-
-console.log(user);
+3 группа:
+4. ${new Date('2024-08-23T15:30:00Z').toISOString()}
+6. ${dayjs('2024-08-23T17:30:00+02:00').toISOString()}
+8. ${dayjs('2024-08-23T03:30:00-12:00').toISOString()}
+10. ${dayjs('2024-08-23T14:30:00-01:00').toISOString()}`);
