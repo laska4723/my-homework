3e41ecb604496c42d84ff8643787d6a6d2cc4e9a
Sergei Rozanov <laska4723@gmail.com>
2025-06-22 19:44:59 +0300

    Составные типы. Задача 7

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,31 @@
-enum Color {
-  GREEN = 'green',
-  RED = 'red',
-  BLUE = 'blue',
-  PURPLE = 'purple'
-}
+import { faker } from '@faker-js/faker';
 
-console.log(Object.values(Color));
+type User = {
+  firstName: string;
+  lastName: string;
+  email: string;
+  birthdate: string;
+  gender: string;
+  country: string;
+  city: string;
+  streetAddress: string;
+  secondaryAddress: string;
+  zipCode: string;
+  phone: string;
+};
+
+const user: User = {
+  firstName: faker.person.firstName(),
+  lastName: faker.person.lastName(),
+  email: faker.internet.email(),
+  birthdate: faker.date.birthdate().toLocaleDateString('ru-RU'),
+  gender: faker.person.sex(),
+  country: faker.location.country(),
+  city: faker.location.city(),
+  streetAddress: faker.location.streetAddress(),
+  secondaryAddress: faker.location.secondaryAddress(),
+  zipCode: faker.location.zipCode('#####'),
+  phone: faker.phone.number({ style: 'international' }),
+};
+
+console.log(user);
