3487f74de7abd9a5ff76a7bbac7d016baf12a13d
Sergei Rozanov <laska4723@gmail.com>
2025-06-27 20:27:57 +0300

    Операторы слияния. Задача 1

--- src/main.ts
+++ src/main.ts
@@ -1,39 +1,12 @@
-import { green, red } from 'chalk';
+type User = { name: string; photo?: string | null };
 
-const tests = [
-  ['топот', true],
-  ['пот', false],
-  ['потоп', true],
-  ['кабак', true],
-  ['(())', false],
-  ['табат', true],
-  ['abab', false],
-  ['топпот', true],
-  ['()()', false],
-  ['', true],
-  ['123321', true],
-  ['())(', true],
-  ['abba', true],
-  ['а роза упала на лапу азора', false], // Пробелы не совпадают!
-] as const;
+const users: User[] = [
+  { name: 'Artem' },
+  { name: 'Kostya', photo: null },
+  { name: 'Elena', photo: '' },
+  { name: 'Alex', photo: 'https://photos.com/Alex.png' },
+];
 
-for (const test of tests) {
-  const [word, expected] = test;
-
-const isPalindrome = (word: string) => {
-  for (let i = 0; i < word.length / 2; i++) {
-    if (word[i] !== word[word.length - 1 - i]) {
-      return false;
-    }
-  }
-  return true;
-}
-
-  const result = isPalindrome(word);
-  if (result !== expected) {
-    console.log(red(`Для слова "${word}" тест не прошел. Ожидалось: ${expected} | Получено: ${result}`));
-    continue;
-  }
-
-  console.log(green(`Слово "${word}" успешно. Результат: ${result}`));
+for (const user of users) {
+  console.log((user.photo ?? 'не обнаружено') || 'пусто');
 }
