952e0e51859c0bdac7b405244f69433af75af00f
Sergei Rozanov <laska4723@gmail.com>
2025-06-28 00:06:16 +0300

    Составные типы. Задача 6 (fix 1)

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,8 @@
-type User = { name: string; photo?: string | null };
-
-const users: User[] = [
-  { name: 'Artem' },
-  { name: 'Kostya', photo: null },
-  { name: 'Elena', photo: '' },
-  { name: 'Alex', photo: 'https://photos.com/Alex.png' },
-];
-
-for (const user of users) {
-  console.log((user.photo ?? 'не обнаружено') || 'пусто');
+enum Color {
+  green = 'green',
+  red = 'red',
+  blue = 'blue',
+  purple = 'purple'
 }
+
+console.log(Object.values(Color));
