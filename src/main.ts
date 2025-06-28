883775f59740ed06e305dfe365c5ffb0dcc84e21
Sergei Rozanov <laska4723@gmail.com>
2025-06-28 15:24:38 +0300

    Составные типы. Задача 8

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,44 @@
+import { fakerRU as faker } from '@faker-js/faker';
+
 enum Color {
-  green = 'green',
-  red = 'red',
-  blue = 'blue',
-  purple = 'purple'
+  Red = 'Red',
+  Green = 'Green',
+  Blue = 'Blue',
 }
 
-console.log(Object.values(Color));
+const colorList: Color[] = [
+  Color.Red,
+  Color.Green,
+  Color.Blue,
+];
+
+type User = {
+  name: string;
+  id: string;
+  email: string;
+  favoriteColor: Color;
+};
+
+const users: User[] = [];
+for (let i = 0; i < 3; i++) {
+  const user: User = {
+    name: faker.person.firstName(),
+    id: faker.string.uuid(),
+    email: faker.internet.email(),
+    favoriteColor: colorList[Math.floor(Math.random() * colorList.length)],
+  };
+  users.push(user)
+}
+
+const randomColor = colorList[Math.floor(Math.random() * colorList.length)];
+
+for (let i = 0; i < users.length; i++) {
+  const user = users[i];
+  if (user.favoriteColor === randomColor) {
+    console.log(`name: ${user.name}
+ID: ${user.id}
+email: ${user.email}
+Любимый цвет: ${user.favoriteColor}`);
+    console.log();
+  }
+}
