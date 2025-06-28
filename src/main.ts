8b55d9e040d7fa8cb1968612aac08706ddddabb9
Sergei Rozanov <laska4723@gmail.com>
2025-06-28 15:32:35 +0300

    Базовые типы. Задача 3 (fix 1)

--- src/main.ts
+++ src/main.ts
@@ -1,44 +1,12 @@
-import { fakerRU as faker } from '@faker-js/faker';
+import { blue, green, red } from 'chalk';
 
-enum Color {
-  Red = 'Red',
-  Green = 'Green',
-  Blue = 'Blue',
+const max = 10;
+const result = [];
+const colors = [red, green, blue];
+
+for (let i = 0; i < max; i++) {
+  const color = colors[i % 3];
+  result.push(color('*'));
 }
 
-const colorList: Color[] = [
-  Color.Red,
-  Color.Green,
-  Color.Blue,
-];
-
-type User = {
-  name: string;
-  id: string;
-  email: string;
-  favoriteColor: Color;
-};
-
-const users: User[] = [];
-for (let i = 0; i < 3; i++) {
-  const user: User = {
-    name: faker.person.firstName(),
-    id: faker.string.uuid(),
-    email: faker.internet.email(),
-    favoriteColor: colorList[Math.floor(Math.random() * colorList.length)],
-  };
-  users.push(user)
-}
-
-const randomColor = colorList[Math.floor(Math.random() * colorList.length)];
-
-for (let i = 0; i < users.length; i++) {
-  const user = users[i];
-  if (user.favoriteColor === randomColor) {
-    console.log(`name: ${user.name}
-ID: ${user.id}
-email: ${user.email}
-Любимый цвет: ${user.favoriteColor}`);
-    console.log();
-  }
-}
+console.log(result.join(' - '));
