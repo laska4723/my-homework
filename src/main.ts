77f2b395f77b0ac7f22e5b109d2a5787b4324797
Sergei Rozanov <laska4723@gmail.com>
2025-07-05 01:54:23 +0300

    Функции. Задача 3

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,33 @@
-const num = 1;
+import { fakerRU as faker } from '@faker-js/faker';
+import { transliterate } from 'transliteration';
 
-const largestProperDivisor = (value: number): number | null => {
-  if (value === 0) {
-    return null;
-  }
-
-  const absValue = Math.abs(value);
-  for (let div = Math.floor(absValue / 2); div > 0; div--) {
-    if (absValue % div === 0) {
-      return div;
-    }
-  }
-
-  return 1;
+type Person = {
+  name: string;
+  age: number;
 };
 
-console.log(`Наибольший собственный делитель числа ${num}:`, largestProperDivisor(num));
+const people: Person[] = [];
+for (let i = 0; i < 10; i++) {
+  people.push({
+    name: faker.person.firstName(),
+    age: faker.number.int({ min: 18, max: 80 })
+  });
+}
+
+const findStasAge = (arr: Person[]): number | string => {
+  for (const person of arr) {
+    const nameInLatin = transliterate(person.name);
+    if (nameInLatin === 'Stas') {
+      if (person.age === null || person.age === undefined) {
+        return 'Возраста нет';
+      } else {
+        return person.age;
+      }
+    }
+  }
+  return 'Объект не найден';
+}
+
+console.log(people);
+console.log();
+console.log('Результат:', findStasAge(people));
