09d16359d29fb40f8e691246a1db5b5be60e792a
Sergei Rozanov <laska4723@gmail.com>
2025-07-12 13:42:55 +0300

    Задача. Пропущенные числа

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,31 @@
-const source = 'the quick brown fox jumps over the lazy dog';
-const custom = 'oak lgypb wited zts qgfch tuki oak mjrn xtv';
-const converted = 'ntg ajuk fjbydv vikjo citvikhh yd mkjidydv qjujhpiyco. ptdvijoh!';
+const getMissingDigits = (arr: number[]): number[] | null => {
+    if (!arr.length) return [];
 
-let result = '';
-for (const char of converted) {
-    result += source[custom.indexOf(char)] ?? char;
-}
-console.log(result);
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] > arr[i + 1]) {
+            return null;
+        }
+    }
+
+    const result: number[] = [];
+    const min = arr[0];
+    const max = arr[arr.length - 1];
+
+    for (let i = min; i <= max; i++) {
+        if (!arr.includes(i)) {
+            result.push(i);
+        }
+    }
+
+    return result;
+};
+
+const result1 = getMissingDigits([1, 2, 3, 5, 8, 10, 11, 13]);
+
+const result2 = getMissingDigits([1, 2, 5, 3]);
+
+const result3 = getMissingDigits([1, 5]);
+
+console.log(result1);
+console.log(result2);
+console.log(result3);
