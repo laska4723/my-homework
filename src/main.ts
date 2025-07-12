2e7056c021a71ebc2b230957485b4c22bc471b70
Sergei Rozanov <laska4723@gmail.com>
2025-07-12 14:14:11 +0300

    Задача. Палиндром

--- src/main.ts
+++ src/main.ts
@@ -1,31 +1,11 @@
-const getMissingDigits = (arr: number[]): number[] | null => {
-    if (!arr.length) return [];
-
-    for (let i = 0; i < arr.length; i++) {
-        if (arr[i] > arr[i + 1]) {
-            return null;
+const palindrome = (str: string): boolean => {
+    for (let i = 0; i < str.length / 2; i++) {
+        if (str[i] !== str[str.length - 1 - i]) {
+            return false;
         }
     }
-
-    const result: number[] = [];
-    const min = arr[0];
-    const max = arr[arr.length - 1];
-
-    for (let i = min; i <= max; i++) {
-        if (!arr.includes(i)) {
-            result.push(i);
-        }
-    }
-
-    return result;
+    return true;
 };
 
-const result1 = getMissingDigits([1, 2, 3, 5, 8, 10, 11, 13]);
-
-const result2 = getMissingDigits([1, 2, 5, 3]);
-
-const result3 = getMissingDigits([1, 5]);
-
-console.log(result1);
-console.log(result2);
-console.log(result3);
+const result = palindrome('кабак');
+console.log(result);
