ca9b42b90fd800d2505a52c94e79a12f94ff7629
Sergei Rozanov <laska4723@gmail.com>
2025-07-13 06:28:48 +0300

    Задача. Степень двойки

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,27 @@
-const palindrome = (str: string): boolean => {
-    for (let i = 0; i < str.length / 2; i++) {
-        if (str[i] !== str[str.length - 1 - i]) {
-            return false;
-        }
+const isPowerOf2 = (value: number): boolean => {
+    let n = 0;
+    while (2 ** n < value) {
+        n++;
     }
-    return true;
-};
+    return 2 ** n === value;
+}
 
-const result = palindrome('кабак');
-console.log(result);
+console.log(isPowerOf2(0) === false);
+console.log(isPowerOf2(1) === true);
+console.log(isPowerOf2(2) === true);
+console.log(isPowerOf2(3) === false);
+console.log(isPowerOf2(4) === true);
+console.log(isPowerOf2(7) === false);
+console.log(isPowerOf2(8) === true);
+console.log(isPowerOf2(120) === false);
+console.log(isPowerOf2(127) === false);
+console.log(isPowerOf2(128) === true);
+console.log(isPowerOf2(500) === false);
+console.log(isPowerOf2(512) === true);
+console.log(isPowerOf2(777) === false);
+console.log(isPowerOf2(1024) === true);
+console.log(isPowerOf2(65000) === false);
+console.log(isPowerOf2(65536) === true);
+console.log(isPowerOf2(520000) === false);
+console.log(isPowerOf2(524288) === true);
+console.log(isPowerOf2(1000000) === false);
