c147820023716d114c50829e9494de7b671cdf8e
Sergei Rozanov <laska4723@gmail.com>
2025-07-17 07:33:35 +0300

    Работа с массивами. Задача 7

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,6 @@
-const storage = [
-    { age: 10, name: 'Alex' },
-    { age: 20, name: 'Max' },
-    { age: 30, name: 'Jake' },
-    { age: 40, name: 'Lilo' },
-];
+function getLongestString(arr: string[]): string {
+    return arr.reduce((a, b) => a.length > b.length ? a : b);
+}
 
-const smartSearch = (arr: any[], property: any, value: any) => {
-    // ... Ваш код здесь
-    return arr.find(el => el[property] === value);
-};
-
-const person1 = smartSearch(storage, 'age', 30);
-// { age: 30, name: 'Jake' }
-
-const person2 = smartSearch(storage, 'age', 10);
-// { age: 10, name: 'Alex' }
-
-const person3 = smartSearch(storage, 'name', 'Lilo');
-// { age: 40, name: 'Lilo' }
-
-console.log(person1);
-console.log(person2);
-console.log(person3);
+const words: string[] = ['ноутбук', 'кресло', 'сумка', 'ключ'];
+console.log(getLongestString(words));
