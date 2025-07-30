7e6b9674e8614c223217f8539d10705b70ffb1ed
Sergei Rozanov <laska4723@gmail.com>
2025-07-30 20:50:42 +0300

    Деструктуризация. Задача 1

--- src/main.ts
+++ src/main.ts
@@ -1,57 +1,11 @@
-/*
-Вам даны 2 пользователя банковской системы.
-У каждого пользователя есть список проведенных операций по банковской карте.
-Каждая операция может быть как положительной (пополнение), так и отрицательной (снятие).
-
-При открытии счета у пользователя есть стартовый баланс - он может быть как отрицательным, так и положительным.
-
-Напишите функцию calculateBalanceForUser(user: User): number, которая принимает на вход объект типа User,
-а возвращает число - текущий баланс пользователя.
-С помощью reduce просто просуммируйте стартовый баланс и все операции.
- */
-
-type Transaction = {
-    id: number;
-    diff: number;
+const mergeToNewObject = (a: Record<string, any>, b: Record<string, any>) => {
+  return { ...a, ...b };
 };
 
-type User = {
-    id: number;
-    name: string;
-    startBalance: number;
-    transactions: Transaction[];
-};
+const obj1 = { a: 10, b: 100, c: 1 };
+const obj2 = { a: 20, b: 200, e: 2 };
+const obj3 = mergeToNewObject(obj1, obj2);
 
-const alexander: User = {
-    id: 37,
-    name: 'AleX',
-    startBalance: 0,
-    transactions: [
-        { id: 8, diff: 100 },
-        { id: 30, diff: -50 },
-        { id: 23, diff: 250 },
-    ],
-};
-
-const max: User = {
-    id: 40,
-    name: 'MaX',
-    startBalance: -3,
-    transactions: [
-        { id: 4, diff: 9 },
-        { id: 5, diff: -18 },
-        { id: 5, diff: 3 },
-    ],
-};
-
-const calculateBalanceForUser = (user: User): number => {
-    return user.transactions.reduce((sum, transaction) => {
-        return sum + transaction.diff;
-    }, user.startBalance);
-};
-
-const alexanderBalance = calculateBalanceForUser(alexander); // Его история операция: 0 + 100 - 50 + 250
-console.log(alexanderBalance); // 300
-
-const maxBalance = calculateBalanceForUser(max); // Его история операция: -3 + 9 - 18 + 3
-console.log(maxBalance); // -9
+console.log(obj1); // { a: 10, b: 100, c: 1 }
+console.log(obj2); // { a: 20, b: 200, e: 2 }
+console.log(obj3); // { a: 20, b: 200, c: 1, e: 2 }
