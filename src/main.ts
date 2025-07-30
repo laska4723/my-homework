6c3aa6cace316c2c224601695387217a455ef367
Sergei Rozanov <laska4723@gmail.com>
2025-07-30 21:13:01 +0300

    Деструктуризация. Задача 4

--- src/main.ts
+++ src/main.ts
@@ -1,25 +1,46 @@
-const storage = [
-    'nick',
-    'nack',
-    'nock',
-    [
-        {
-            first: 'forecast',
-            child: null,
+const megaobject = {
+    child: null,
+    child1: {
+        nick: 'name',
+        nicknames: ['n1', 'n2', { n: 'n', b: 'b' }],
+        james: {
+            kates: {
+                nicks: {
+                    nicknames: ['nn', 'vv', 'gg'],
+                    child: {
+                        nick: 'childName',
+                        child: undefined,
+                    },
+                },
+            },
         },
-        {
-            first: 'castfore',
-            child: null,
-        },
-        'zzz',
-    ],
-    'no-1',
-    'no-2',
-] as const;
+    },
+    child2: {},
+    child3: 'child3',
+};
 
-const [ , , , [b0, b1, cos0], ...nos ] = storage;
-const cos = [cos0];
+const {
+    child,
+    child1: {
+        nick,
+        nicknames,
+        james: {
+            kates: {
+                nicks: {
+                    nicknames: [nicknames1, ...otherNicks],
+                    child: { nick: nick1, child: child1 }
+                }
+            }
+        }
+    },
+    ...otherChilds
+} = megaobject;
 
-console.log(nos);
-console.log(cos);
-console.log(b1);
+console.log(child);       // null
+console.log(otherChilds); // { child2: {}, child3: 'child3' }
+console.log(nick);        // name
+console.log(nicknames);   // [ 'n1', 'n2', { n: 'n', b: 'b' } ]
+console.log(nicknames1);  // nn
+console.log(otherNicks);  // [ 'vv', 'gg' ]
+console.log(nick1);       // childName
+console.log(child1);      // undefined
