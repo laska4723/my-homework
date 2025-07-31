083ad17dbb6c67bb5d075b35751ed67d9ecfe4ce
Sergei Rozanov <laska4723@gmail.com>
2025-07-31 06:48:50 +0300

    Generics. Задача 1

--- src/main.ts
+++ src/main.ts
@@ -1,46 +1,24 @@
-const megaobject = {
-    child: null,
-    child1: {
-        nick: 'name',
-        nicknames: ['n1', 'n2', { n: 'n', b: 'b' }],
-        james: {
-            kates: {
-                nicks: {
-                    nicknames: ['nn', 'vv', 'gg'],
-                    child: {
-                        nick: 'childName',
-                        child: undefined,
-                    },
-                },
-            },
-        },
-    },
-    child2: {},
-    child3: 'child3',
+type Paginated<T> = {
+    total: number;
+    limit: number;
+    offset: number;
+    items: T[];
 };
 
-const {
-    child,
-    child1: {
-        nick,
-        nicknames,
-        james: {
-            kates: {
-                nicks: {
-                    nicknames: [nicknames1, ...otherNicks],
-                    child: { nick: nick1, child: child1 }
-                }
-            }
-        }
-    },
-    ...otherChilds
-} = megaobject;
+type Person = {
+    name: string;
+};
 
-console.log(child);       // null
-console.log(otherChilds); // { child2: {}, child3: 'child3' }
-console.log(nick);        // name
-console.log(nicknames);   // [ 'n1', 'n2', { n: 'n', b: 'b' } ]
-console.log(nicknames1);  // nn
-console.log(otherNicks);  // [ 'vv', 'gg' ]
-console.log(nick1);       // childName
-console.log(child1);      // undefined
+const responseOfPersons: Paginated<Person> = {
+    total: 10,
+    limit: 5,
+    offset: 0,
+    items: [{ name: 'P1' }],
+};
+
+const responseOfSomething: Paginated<{}> = {
+    total: 10,
+    limit: 5,
+    offset: 0,
+    items: [],
+};
