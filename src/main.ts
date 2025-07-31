c1267f5f53bbc27a6346998a3c7994b7d31a094d
Sergei Rozanov <laska4723@gmail.com>
2025-07-31 07:09:10 +0300

    Generics. Задача 4

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,64 @@
-import { faker } from '@faker-js/faker';
+/**
+ * Создайте дженерик-функцию "sortById", которая может сортировать массивы любых типов, у которых есть { id: number }.
+ * Можно выбирать направление сортировки - asc (по возрастанию) и desc (по убыванию).
+ * По умолчанию используется по возрастанию.
+ *
+ * Если вы забыли про сортировку, то вспомните тему Массивы. map, find, filter, sort, reduce.
+ */
 
-const parse = <T>(json: string): T => JSON.parse(json);
+import { fakerRU } from '@faker-js/faker';
 
 type User = {
     id: number;
     name: string;
 };
 
-const user: User = {
-    id: faker.number.int(),
-    name: faker.person.fullName(),
+type Animal = {
+    id: number;
+    title: string;
+    type: string;
 };
 
-const userString = JSON.stringify(user);
+const randomUsers: User[] = Array.from({ length: 5 }).map(() => {
+    return {
+        id: fakerRU.number.int({ min: 1, max: 100 }),
+        name: fakerRU.person.fullName(),
+    };
+});
 
-const parsedUser = parse<User>(userString);
+const randomAnimals: Animal[] = Array.from({ length: 7 }).map(() => {
+    return {
+        id: fakerRU.number.int({ min: 1, max: 100 }),
+        title: fakerRU.person.fullName(),
+        type: fakerRU.animal.type(),
+    };
+});
 
-console.log(parsedUser.name);
+const randomObjects = Array.from({ length: 4 }).map(() => {
+    return {
+        id: fakerRU.number.int({ min: 1, max: 100 }),
+        company: fakerRU.company.name(),
+        food: fakerRU.food.dish(),
+    };
+});
+
+const sortById = <T extends { id: number }>(items: T[], direction: 'asc' | 'desc' = 'asc') => {
+    items.sort((a, b) => (direction === 'asc' ? a.id - b.id : b.id - a.id));
+};
+
+sortById(randomUsers);
+console.log(randomUsers); // Должны быть отсортированы по возрастанию id
+
+sortById(randomAnimals, 'desc');
+console.log(randomAnimals); // Должны быть отсортированы по убыванию id
+
+sortById(randomObjects, 'asc');
+console.log(randomObjects); // Должны быть отсортированы по возрастанию id
+
+/*
+В примерах ниже должна гореть ошибка!
+Для запуска закомментируйте эти строки
+ */
+sortById([{}]);
+sortById([{ a: 10 }]);
+sortById([{ id: 'asj' }]);
