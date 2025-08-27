7b9700e87afc82b62ce0ea7ba741aca280a05e9f
Sergei Rozanov <laska4723@gmail.com>
2025-08-27 21:09:53 +0300

    Создание проекта. Пакеты. Задача 1

--- main.js
+++ main.js
@@ -1,41 +1,6 @@
-/*
-Дан массив из 10 объектов с пользователями.
-У каждого пользователя есть имя и возраст.
+import chalk from 'chalk';
 
-Подсчитайте и выведите количество различных возрастов.
-
-Вы должны повторить такой вывод:
-Возраст=25. Количество людей=3
-Возраст=28. Количество людей=2
-Возраст=30. Количество людей=3
-Возраст=35. Количество людей=1
-Возраст=41. Количество людей=1
- */
-
-const users = [
-  { name: 'Alice', age: 25 },
-  { name: 'Bob', age: 30 },
-  { name: 'Charlie', age: 25 },
-  { name: 'Diana', age: 35 },
-  { name: 'Eve', age: 30 },
-  { name: 'Frank', age: 28 },
-  { name: 'Grace', age: 25 },
-  { name: 'Hannah', age: 41 },
-  { name: 'Ivy', age: 30 },
-  { name: 'Jack', age: 28 },
-];
-
-const count = {}; // Подсказка
-
-// Ваш код здесь ...
-for (const user of users) {
-  const age = user.age;
-  if (count[age] === undefined) {
-    count[age] = 0;
-  }
-  count[age]++;
-}
-
-for (const key in count) {
-  console.log(`Возраст=${key}. Количество людей=${count[key]}`);
-}
+console.log(chalk.bgBlue.grey(new Date().toISOString()));
+console.log(chalk.bgBlack.green(new Date().toISOString()));
+console.log(chalk.bgGray.red(new Date().toISOString()));
+console.log(chalk.bgGreen.blue(new Date().toISOString()));
