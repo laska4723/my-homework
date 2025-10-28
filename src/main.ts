e5dc26bb188a1a26f5793b236e846428e4bf602b
Sergei Rozanov <laska4723@gmail.com>
2025-10-28 09:59:25 +0300

    Задача. Палиндром Число (ver. 2)

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,25 @@
-/* Самая длинная строка в массиве
-Напишите функцию, которая получает на вход массив строк и возвращает самую длинную строку из массива.
-Для решения используйте reduce.
+/* Палиндром Число
+Напишите функцию, которая получает на вход 2 числа и возвращает true / false - является читаются ли эти числа наоборот.
+Конвертировать число в строку запрещено! Работайте с входными данными именно как с числом! Например:
+
+f(123, 321) // true, 123 это 321 наоборот
+f(22, 22) // true, 22 это 22 наоборот
+f(56, 56) // false, 56 наоборот это НЕ 56
 */
 
-const longestString = (arr: string[]): string => {
-  return arr.reduce((longest, current) =>
-      current.length > longest.length ? current : longest
-    , "");
+const palindromeNum = (a: number, b: number): boolean => {
+  let originalNum = a;
+  let reversedNum = 0;
+
+  while (originalNum > 0) {
+    let num = originalNum % 10;
+    reversedNum = reversedNum * 10 + num;
+    originalNum = Math.floor(originalNum / 10);
+  }
+
+  return reversedNum === b;
 }
 
-// Пример:
-const strings = ["apple", "banana", "kiwi", "pineapple", "orange"];
-console.log(longestString(strings)); // "pineapple"
+console.log(palindromeNum(123, 321)); // true
+console.log(palindromeNum(22, 22));   // true
+console.log(palindromeNum(56, 56));   // false
