da060fbe4adcf469c024b9d8383c990547f3d1c7
Sergei Rozanov <laska4723@gmail.com>
2025-10-28 15:47:36 +0300

    Задача. Сумма элементов массива

--- src/main.ts
+++ src/main.ts
@@ -1,25 +1,12 @@
-/* Палиндром Число
-Напишите функцию, которая получает на вход 2 числа и возвращает true / false - является читаются ли эти числа наоборот.
-Конвертировать число в строку запрещено! Работайте с входными данными именно как с числом! Например:
-
-f(123, 321) // true, 123 это 321 наоборот
-f(22, 22) // true, 22 это 22 наоборот
-f(56, 56) // false, 56 наоборот это НЕ 56
+/* Сумма элементов массива
+Напишите функцию, которая получает на вход массив чисел и возвращает их сумму.
+Для решения используйте reduce.
 */
 
-const palindromeNum = (a: number, b: number): boolean => {
-  let originalNum = a;
-  let reversedNum = 0;
-
-  while (originalNum > 0) {
-    let num = originalNum % 10;
-    reversedNum = reversedNum * 10 + num;
-    originalNum = Math.floor(originalNum / 10);
-  }
-
-  return reversedNum === b;
+const sumArray = (arr: number[]): number => {
+  return arr.reduce((sum, current) => sum += current);
 }
 
-console.log(palindromeNum(123, 321)); // true
-console.log(palindromeNum(22, 22));   // true
-console.log(palindromeNum(56, 56));   // false
+// Пример:
+const arr = [2, 5, 7, 4, 8];
+console.log(sumArray(arr)); // 26
